4ed0560d0991df699e859c66faf34cbbe5da174b
Kye <kye@apacmediasolutions.com>
2024-01-31 20:45:06 -0500

    cleanup

--- example.py
+++ example.py
@@ -2,9 +2,6 @@ from vision_datasets.iter_over_dataset import ImageDatasetIterator
 from swarms import QwenVLMultiModal
 
 model = QwenVLMultiModal(
-<<<<<<< HEAD
-    system_prompt="You, as the model, are presented with a visual problem. This could be an image containing various elements that you need to analyze, a graph that requires interpretation, or a visual puzzle. Your task is to examine the visual information carefully and describe your process of understanding and solving the problem.",
-=======
     system_prompt=(
         "You, as the model, are presented with a visual problem. This"
         " could be an image containing various elements that you need"
@@ -13,7 +10,6 @@ model = QwenVLMultiModal(
         " information carefully and describe your process of"
         " understanding and solving the problem."
     ),
->>>>>>> a7d64d7 ([FEAT][create_base_retry_decorator])
 )
 
 iterator = ImageDatasetIterator("coco", model)
